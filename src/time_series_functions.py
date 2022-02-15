54fc382ce23ee5de88299ce7ca165291fd6d44ef
Axel <axelvieira94@gmail.com>
2022-02-14 21:15:18 -0300

    Alteração para salvar - #Task-1

---
+++ src/time_series_functions.py
@@ -0,0 +1,157 @@
+import pandas as pd
+import numpy as np
+import pickle as pkl
+
+
+def create_windowing(df, lag_size):
+    final_df = None
+    for i in range(0, (lag_size + 1)):
+        serie = df.shift(i)
+        if (i == 0):
+            serie.columns = ['actual']
+        else:
+            serie.columns = [str('lag' + str(i))]
+        final_df = pd.concat([serie, final_df], axis=1)
+
+    return final_df.dropna()
+
+def mean_square_error(y_true, y_pred):
+    y_true = np.asmatrix(y_true).reshape(-1)
+    y_pred = np.asmatrix(y_pred).reshape(-1)
+
+    return np.square(np.subtract(y_true, y_pred)).mean()
+
+def root_mean_square_error(y_true, y_pred):
+
+    return mean_square_error(y_true, y_pred)**0.5
+
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    y_true = np.asarray(y_true).reshape(-1)
+    y_pred = np.asarray(y_pred).reshape(-1)
+
+    if len(np.where(y_true == 0)[0]) > 0:
+        return np.inf
+    else:
+        return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+def symmetric_mean_absolute_percentage_error(y_true, y_pred):
+    y_true = np.asarray(y_true).reshape(-1)
+    y_pred = np.asarray(y_pred).reshape(-1)
+
+    return np.mean(np.abs((y_true - y_pred) / (( np.abs(y_true) + np.abs(y_pred) )/2) ))
+
+def mean_absolute_error(y_true, y_pred):
+    
+    y_true = np.asarray(y_true).reshape(-1)
+    y_pred = np.asarray(y_pred).reshape(-1)
+
+    return np.mean(np.abs(y_true - y_pred))
+
+
+def u_theil(y_true, y_pred):
+    y_true = np.asarray(y_true).reshape(-1)
+    y_pred = np.asarray(y_pred).reshape(-1)
+
+    error_sup = np.square(np.subtract(y_true, y_pred)).sum()
+    error_inf = np.square(np.subtract(y_pred[0:(len(y_pred) - 1)], y_pred[1:(len(y_pred))])).sum()
+
+    return error_sup / error_inf
+
+
+def average_relative_variance(y_true, y_pred):
+    y_true = np.asarray(y_true).reshape(-1)
+    y_pred = np.asarray(y_pred).reshape(-1)
+    mean = np.mean(y_true)
+
+    error_sup = np.square(np.subtract(y_true, y_pred)).sum()
+    error_inf = np.square(np.subtract(y_pred, mean)).sum()
+
+    return error_sup / error_inf
+
+
+def index_agreement(y_true, y_pred):
+    y_true = np.asarray(y_true).reshape(-1)
+    y_pred = np.asarray(y_pred).reshape(-1)
+    mean = np.mean(y_true)
+
+    error_sup = np.square(np.abs(np.subtract(y_true, y_pred))).sum()
+
+    error_inf = np.abs(np.subtract(y_pred, mean)) + np.abs(np.subtract(y_true, mean))
+    error_inf = np.square(error_inf).sum()
+
+    return 1 - (error_sup / error_inf)
+
+
+def prediction_of_change_in_direction(y_true, y_pred):
+    y_true = np.asarray(y_true).reshape(-1)
+    y_pred = np.asarray(y_pred).reshape(-1)
+
+    true_sub = np.subtract(y_true[0:(len(y_true) - 1)], y_true[1:(len(y_true))])
+    pred_sub = np.subtract(y_pred[0:(len(y_pred) - 1)], y_pred[1:(len(y_pred))])
+
+    mult = true_sub * pred_sub
+    result = 0
+    for m in mult:
+        if m > 0:
+            result = result + 1
+
+    return (100 * (result / len(y_true)))
+
+
+def gerenerate_metric_results(y_true, y_pred):
+    return {'MSE': mean_square_error(y_true, y_pred),
+            'RMSE':root_mean_square_error(y_true, y_pred),
+            'MAPE': mean_absolute_percentage_error(y_true, y_pred),
+            'SMAPE':symmetric_mean_absolute_percentage_error(y_true, y_pred),
+            'MAE': mean_absolute_error(y_true, y_pred),
+            'theil': u_theil(y_true, y_pred),
+            'ARV': average_relative_variance(y_true, y_pred),
+            'IA': index_agreement(y_true, y_pred),
+            'POCID': prediction_of_change_in_direction(y_true, y_pred)}
+
+def make_metrics_avaliation(y_true, y_pred, test_size,
+                            val_size, model_params):
+    data_size = len(y_true)
+    train_size = data_size - (val_size + test_size)
+
+    y_true = np.asarray(y_true).reshape(-1)
+    y_pred = np.asarray(y_pred).reshape(-1)
+
+    y_true_test = y_true[(data_size - test_size):data_size]
+    y_pred_test = y_pred[(data_size - test_size):data_size]
+
+    val_result = None
+
+    if val_size>0:
+        y_true_val = y_true[(train_size):(data_size - test_size)]
+        y_pred_val = y_pred[(train_size):(data_size - test_size)]
+        val_result = gerenerate_metric_results(y_true_val, y_pred_val)
+
+    y_true_train = y_true[:train_size]
+    y_pred_train = y_pred[:train_size]
+
+    geral_dict = {
+        'test_metrics': gerenerate_metric_results(y_true_test, y_pred_test),
+        'val_metrics': val_result,
+        'train_metrics': gerenerate_metric_results(y_true_train, y_pred_train),
+        'real_values': y_true,
+        'predicted_values': y_pred,
+        'params': model_params
+    }
+
+    return geral_dict
+
+def save_result(dict_result, title):
+
+    title = title+".pkl"
+
+    with open(title, 'wb') as handle:
+        pkl.dump(dict_result, handle)
+
+    return title
+
+def open_saved_result(file_name):
+    with open(file_name, 'rb') as handle:
+        b = pkl.load(handle)
+    return b
